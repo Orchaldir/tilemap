c7ea1d7d67747bf838a0ab19ccba4e855cee3c88
Orchaldir <orchaldir@gmail.com>
2022-09-18 14:17:07 +0200

    [#1] Import Tilemap2d

---
+++ tilemap/src/tilemap/tile.rs
@@ -0,0 +1,9 @@
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum Tile {
+    /// Empty like a hole to a lower level or the empty sky around a flying island.
+    Empty,
+    /// The ground outside or the floor of a building.
+    Floor(usize),
+    /// Full of a solid material like earth or stone. E.g. underground
+    Solid(usize),
+}

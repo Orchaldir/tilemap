54d0b7a3eef3219823dd0e03dd9defa697cdc91d
Orchaldir <orchaldir@gmail.com>
2022-09-24 22:15:48 +0200

    [#4] Add SvgBuilder.rectangle()

--- tilemap_svg/src/renderer.rs
+++ tilemap_svg/src/renderer.rs
@@ -1,3 +1,4 @@
+use tilemap::math::color::Color;
 use tilemap::math::size2d::Size2d;
 
 /// A valid [SVG](https://en.wikipedia.org/wiki/Scalable_Vector_Graphics).
@@ -31,6 +32,17 @@ impl SvgBuilder {
         Self { lines }
     }
 
+    pub fn rectangle(&mut self, x: u32, y: u32, size: Size2d, color: Color) {
+        self.lines.push(format!(
+            "  <rect x=\"{}\" y=\"{}\" width=\"{}\" height=\"{}\" fill=\"{}\"/>",
+            x,
+            y,
+            size.width(),
+            size.height(),
+            color.to_hex(),
+        ));
+    }
+
     pub fn finish(mut self) -> Svg {
         self.lines.push("</svg>".to_string());
 
@@ -41,6 +53,7 @@ impl SvgBuilder {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tilemap::math::color::{ORANGE, PINK};
 
     #[test]
     fn test_empty_svg() {
@@ -48,6 +61,21 @@ mod tests {
         let svg = builder.finish();
 
         let result = "<svg viewBox=\"0 0 100 150\" xmlns=\"http://www.w3.org/2000/svg\">\n</svg>";
+
+        assert_eq!(&svg.export(), result);
+    }
+
+    #[test]
+    fn test_rectangles() {
+        let mut builder = SvgBuilder::new(Size2d::new(100, 150));
+        builder.rectangle(10, 20, Size2d::new(30, 40), ORANGE);
+        builder.rectangle(50, 70, Size2d::new(35, 45), PINK);
+        let svg = builder.finish();
+
+        let result = "<svg viewBox=\"0 0 100 150\" xmlns=\"http://www.w3.org/2000/svg\">
+  <rect x=\"10\" y=\"20\" width=\"30\" height=\"40\" fill=\"#FFA500\"/>
+  <rect x=\"50\" y=\"70\" width=\"35\" height=\"45\" fill=\"#FF0080\"/>
+</svg>";
 
         assert_eq!(&svg.export(), result);
     }

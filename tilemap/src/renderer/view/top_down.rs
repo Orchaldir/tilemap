afbcf283bb546c66b041837ae03b196641b46e5a
Orchaldir <orchaldir@gmail.com>
2022-10-06 19:45:49 +0200

    [#17] Add TopDownView.render_horizontal_borders()

--- tilemap/src/renderer/view/top_down.rs
+++ tilemap/src/renderer/view/top_down.rs
@@ -4,6 +4,7 @@ use crate::math::size2d::Size2d;
 use crate::port::renderer::Renderer;
 use crate::renderer::style::Style;
 use crate::renderer::view::View;
+use crate::tilemap::border::{get_horizontal_borders_size, Border};
 use crate::tilemap::tile::Tile;
 use crate::tilemap::tilemap2d::Tilemap2d;
 
@@ -19,6 +20,7 @@ impl View for TopDownView {
 
     fn render(&self, tilemap: &Tilemap2d, renderer: &mut dyn Renderer, style: &Style) {
         self.render_tiles(tilemap, renderer, style);
+        self.render_horizontal_borders(tilemap, renderer, style);
     }
 
     fn render_grid(&self, tiles: Size2d, renderer: &mut dyn Renderer, style: &Style) {
@@ -71,6 +73,43 @@ impl TopDownView {
             }
 
             y += self.tile_size.height();
+        }
+    }
+
+    fn render_horizontal_borders(
+        &self,
+        tilemap: &Tilemap2d,
+        renderer: &mut dyn Renderer,
+        style: &Style,
+    ) {
+        let size = get_horizontal_borders_size(tilemap.get_size());
+        let borders = tilemap.get_horizontal_borders();
+
+        let mut y = 0i32;
+        let mut index = 0;
+
+        for _y in 0..size.height() {
+            let mut x = 0;
+
+            for _x in 0..size.width() {
+                match &borders[index] {
+                    Border::Empty => {}
+                    Border::Wall(_) => {
+                        let thickness = style.get_wall_thickness();
+                        renderer.render_rectangle(
+                            x,
+                            (y - thickness as i32 / 2) as u32,
+                            Size2d::new(self.tile_size.width(), thickness),
+                            *style.get_top_color(),
+                        )
+                    }
+                }
+
+                x += self.tile_size.width();
+                index += 1;
+            }
+
+            y += self.tile_size.height() as i32;
         }
     }
 
--- tilemap_svg/examples/map.rs
+++ tilemap_svg/examples/map.rs
@@ -2,17 +2,35 @@ extern crate tilemap;
 extern crate tilemap_svg;
 
 use tilemap::math::color::{BLACK, CYAN, GREEN, RED, YELLOW};
+use tilemap::math::side::Side;
 use tilemap::math::size2d::Size2d;
 use tilemap::renderer::style::Style;
 use tilemap::renderer::view::isometric::IsometricView;
 use tilemap::renderer::view::three_four::ThreeFourView;
 use tilemap::renderer::view::top_down::TopDownView;
 use tilemap::renderer::view::View;
+use tilemap::tilemap::border::Border;
 use tilemap::tilemap::tile::Tile;
 use tilemap::tilemap::tilemap2d::Tilemap2d;
 use tilemap_svg::renderer::SvgBuilder;
 
 fn main() {
+    let tilemap = create_wall_example();
+
+    let tile_side = 100;
+    let tile_size = Size2d::square(tile_side);
+    let height = 200;
+
+    let isometric = IsometricView::new(tile_side, height);
+    let three_four = ThreeFourView::new(tile_size, height);
+    let top_down = TopDownView::new(tile_size);
+
+    render(&isometric, &tilemap, "test_isometric.svg");
+    render(&three_four, &tilemap, "test_34.svg");
+    render(&top_down, &tilemap, "test_top.svg");
+}
+
+fn create_tile_example() -> Tilemap2d {
     let tiles = Size2d::new(3, 3);
     let mut tilemap = Tilemap2d::default(tiles, Tile::Empty).unwrap();
 
@@ -27,17 +45,19 @@ fn main() {
         );
     }
 
-    let tile_side = 100;
-    let tile_size = Size2d::square(tile_side);
-    let height = 200;
+    tilemap
+}
 
-    let isometric = IsometricView::new(tile_side, height);
-    let three_four = ThreeFourView::new(tile_size, height);
-    let top_down = TopDownView::new(tile_size);
+fn create_wall_example() -> Tilemap2d {
+    let tiles = Size2d::new(12, 6);
+    let mut tilemap = Tilemap2d::default(tiles, Tile::Floor(0)).unwrap();
 
-    render(&isometric, &tilemap, "test_isometric.svg");
-    render(&three_four, &tilemap, "test_34.svg");
-    render(&top_down, &tilemap, "test_top.svg");
+    tilemap.set_border(32, Side::Back, Border::Wall(0));
+    tilemap.set_border(32, Side::Left, Border::Wall(0));
+    tilemap.set_border(32, Side::Front, Border::Wall(0));
+    tilemap.set_border(32, Side::Right, Border::Wall(0));
+
+    tilemap
 }
 
 fn render(viewer: &dyn View, tilemap: &Tilemap2d, path: &str) {
